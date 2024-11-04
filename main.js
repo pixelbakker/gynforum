e796efd8c2deb32326dc3e5489f2da5c8c2adc32
leifarnebakkerNAV <77058637+leifarnebakkerNAV@users.noreply.github.com>
2024-11-04 20:18:05 +0100

    Changed config for partials

--- main.js
+++ main.js
@@ -10,5 +10,5 @@ async function loadPartial(id, url) {
   document.getElementById(id).innerHTML = text;
 }
 
-loadPartial("header", "/src/partials/header.html");
-loadPartial("footer", "/src/partials/footer.html");
+loadPartial("header", "/partials/header.html");
+loadPartial("footer", "/partials/footer.html");
